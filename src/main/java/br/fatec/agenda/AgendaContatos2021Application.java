5389cfb8e8e3263c0e01fa2945b3caea3a2bb3e2
Ciro Cirne Trindade <ciroct@unisantos.br>
2021-02-25 22:30:49 -0300

    Agenda de Contatos

---
+++ src/main/java/br/fatec/agenda/AgendaContatos2021Application.java
@@ -0,0 +1,13 @@
+package br.fatec.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaContatos2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaContatos2021Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/fatec/agenda/service/ContatoService.java
@@ -0,0 +1,52 @@
+package br.fatec.agenda.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.fatec.agenda.model.Contato;
+import br.fatec.agenda.repositories.ContatoRepository;
+
+@Service
+public class ContatoService implements ServiceInterface<Contato> {
+
+	@Autowired
+	private ContatoRepository repository;
+	
+	@Override
+	public Contato create(Contato obj) {		
+		return repository.save(obj);
+	}
+
+	@Override
+	public Contato findById(Long id) {
+		Optional<Contato> _contato = repository.findById(id);
+		return _contato.orElse(null);
+	}
+
+	@Override
+	public List<Contato> findAll() {	
+		return repository.findAll();
+	}
+
+	@Override
+	public boolean update(Contato obj) {
+		if (repository.existsById(obj.getId())) {
+			repository.save(obj);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delete(Long id) {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+}
