573cc8079a6615a631e9b1b5fa281ef70b2376bf
Ciro Cirne Trindade <ciroct@unisantos.br>
2021-03-04 21:53:11 -0300

    AbstractEntity e Heroku

--- src/main/java/br/fatec/agenda/model/Contato.java
+++ src/main/java/br/fatec/agenda/model/Contato.java
@@ -1,22 +1,14 @@
 package br.fatec.agenda.model;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_contato")
-public class Contato implements Serializable {
+public class Contato extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	@Column(length = 60)
 	private String email;
 	@Column(length = 80)
@@ -25,14 +17,6 @@ public class Contato implements Serializable {
 	private String telefone;
 	
 	public Contato() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getEmail() {
@@ -58,30 +42,5 @@ public class Contato implements Serializable {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contato other = (Contato) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}		
 
 }
